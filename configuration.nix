25573e1a6c6665732af1cd11ed362884564f0bd5
silky <noonsilk+-noonsilk@gmail.com>
2023-02-09 13:53:21 +0000

    god knows

--- configuration.nix
+++ configuration.nix
@@ -194,9 +194,8 @@
 
   nix.settings.trusted-users = [ "root" "noon" ];
 
-  # Let's see if we can get bluetooth working.
   hardware.bluetooth.enable = true;
-  hardware.bluetooth.package = pkgs.bluez;
+  hardware.bluetooth.package = pkgs.bluezFull;
   hardware.bluetooth.hsphfpd.enable = true;
 
   services.blueman.enable = true;
@@ -206,37 +205,27 @@
     enableGnomeKeyring = true;
   };
 
+  environment.etc = { "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+      bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+      }
+    '';
+  };
+
   # rtkit is optional but recommended
-  sound.enable = false;
   security.rtkit.enable = true;
+
+  sound.enable = false;
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # High quality BT calls
-    media-session.config.bluez-monitor.rules = [
-      {
-        # Matches all cards
-        matches = [{ "device.name" = "~bluez_card.*"; }];
-        actions = {
-          "update-props" = {
-            "bluez5.auto-connect" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
-          };
-        };
-      }
-      {
-        matches = [
-          # Matches all sources
-          { "node.name" = "~bluez_input.*"; }
-          # Matches all outputs
-          { "node.name" = "~bluez_output.*"; }
-        ];
-        actions = {
-          "node.pause-on-idle" = false;
-        };
-      }
-    ];
+    wireplumber.enable = true;
   };
 
 
