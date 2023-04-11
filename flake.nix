42122ecb5e93bdfbbde9a377190269389187d8eb
silky <noonsilk+-noonsilk@gmail.com>
2023-04-11 14:10:22 +0100

    cleanup

--- flake.nix
+++ flake.nix
@@ -1,15 +1,9 @@
 {
   inputs = {
-    nixpkgs.url  = "nixpkgs/nixos-22.11";
-    unstable.url = "nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
-    };
-
-    nixos-hardware = {
-      url = "github:NixOS/nixos-hardware/master";
-    };
+    nixpkgs.url        = "nixpkgs/nixos-22.11";
+    unstable.url       = "nixpkgs/nixos-unstable";
+    home-manager.url   = "github:nix-community/home-manager/release-22.11";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
 
@@ -24,7 +18,6 @@
 
           ./machines/${name}/configuration.nix
 
-          # Home-manager stuffs.
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
@@ -39,23 +32,5 @@
       user     = "noon";
       overlays = [];
     };
-
-
-    # nixosConfigurations.nqpac = nixpkgs.lib.nixosSystem {
-    #   system      = "x86_64-linux";
-    #   specialArgs = attrs;
-    #   modules     = [
-    #     nixos-hardware.nixosModules.lenovo-thinkpad-x1-6th-gen
-
-    #     ./configuration.nix
-
-    #     home-manager.nixosModules.home-manager {
-    #       home-manager.useGlobalPkgs = true;
-    #       home-manager.useUserPackages = true;
-    #       home-manager.users.noon = import ./home.nix;
-    #       home-manager.extraSpecialArgs = { inherit unstable; };
-    #     }
-    #   ];
-    # };
   };
 }
