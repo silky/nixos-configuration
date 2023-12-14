42b1b1a863a7234a64644b2a999c159da104f4f8
silky <noonsilk+-noonsilk@gmail.com>
2023-12-14 16:25:11 +0000

    holiday

--- users/noon/common-configuration.nix
+++ users/noon/common-configuration.nix
@@ -182,7 +182,8 @@ in
   # ~ Internationalisation / i18n
   #
   # ---------------------------------------------------------------------------
-  time.timeZone      = "Europe/London";
+  # time.timeZone      = "Europe/London";
+  time.timeZone      = "America/Mexico_City";
   i18n.defaultLocale = "en_GB.utf8";
 
 
