9f3ec167c5beede1865df660cae769e8a469e6d3
Noon van der Silk <noonsilk+-noonsilk@gmail.com>
2024-08-13 08:45:58 +0100

    hls wip

--- users/noon/haskell.lua
+++ users/noon/haskell.lua
@@ -3,10 +3,9 @@ vim.g.haskell_tools = {
     on_attach = function(client, bufnr, ht)
       -- local bufopts = { noremap = true, buffer = bufnr }
       require("which-key").add({
-        { "gd", vim.lsp.buf.definition, desc = "goto definition" },
-        { "gr", vim.lsp.buf.references, desc = "references" },
-        { "K",  vim.lsp.buf.hover, desc = "hover" },
-        { "<localleader>hs", ht.hoogle.hoogle_signature, desc = "hoogle signature" },
+        { "gd", vim.lsp.buf.definition, desc = "Goto definition" },
+        { "gr", vim.lsp.buf.references, desc = "Show all references" },
+        { "K",  vim.lsp.buf.hover, desc = "Hover info" },
         -- { "cl", vim.lsp.codelens.run, desc = "code lens" },
         -- { "gD", vim.lsp.buf.declaration, desc = "goto declaration" },
         -- { "gi", vim.lsp.buf.implementation, desc = "goto implementation" },
@@ -14,20 +13,3 @@ vim.g.haskell_tools = {
     end,
   }
 }
-
-
---     -- Show line diagnostics automatically in hover window
---       -- floatDiag = function()
---       --   local opts = {
---       --     focusable = false,
---       --     close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
---       --     border = "rounded",
---       --     source = "always",
---       --     prefix = " ",
---       --     scope = "cursor",
---       --   }
---       --   vim.diagnostic.open_float(nil, opts)
---       -- end
-
---   end
--- }
