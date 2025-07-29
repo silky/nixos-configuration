8bd76cc250a42cbc4ffc508ce2fa7e55e83d035c
Noon van der Silk <noonsilk+-noonsilk@gmail.com>
2025-07-28 20:44:01 -0600

    stable

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
 
     # Home-manager also needs to be unstable.
-    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
